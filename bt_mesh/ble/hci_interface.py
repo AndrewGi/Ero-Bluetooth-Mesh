a5b4d552ce16e8ef6775ff2265e262ad21e13648
Andrew <andrew@gilbrough.com>
2019-11-05 14:27:57 -0800

    added BLE events to hci.py

--- bt_mesh/ble/hci_interface.py
+++ bt_mesh/ble/hci_interface.py
@@ -1,5 +1,4 @@
-from . import hci
-
+from . import hci, adapter, hci_config_parameters
 
 class Interface:
 	def open(self) -> None:
@@ -13,3 +12,20 @@ class Interface:
 
 	def write(self, b: bytes) -> None:
 		raise NotImplementedError()
+
+	def execute_command(self, command: hci.Command):
+		raise NotImplementedError()
+
+	def handle_hci_event(self, event: hci.Event) -> None:
+		if event == hci.
+
+	def advertise_enable(self, is_enabled: bool) -> None:
+		self.execute_command(hci_config_parameters.AdvertisingEnable(is_enabled).as_command())
+
+	def reset(self) -> None:
+		self.execute_command(hci_config_parameters.)
+
+
+class HCIAdapter(adapter.Adapter):
+	def reset(self):
+		self
